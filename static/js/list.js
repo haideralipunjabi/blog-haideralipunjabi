0ae991142eb00aa4895c193cce851a4c4758ad6d
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-12-27 20:16:34 +0530

    Added GDPR

---
+++ static/js/list.js
@@ -0,0 +1,16 @@
+const old_data = {
+    "adding-view-count-to-your-jamstack-website":156,
+    "automating-android-game-with-python-pytesseract-sudoku":132,
+    "clean-efficient-coding-an-example-for-beginners":2061,
+    "country-flag-visualisation": 71,
+    "dragon-ball-scouter-font": 399,
+    "harry-potter-books-fanfiction-an-analysis-of-words": 259,
+    "making-hpfanfiction-recommender": 35,
+    "making-modules-for-polybar-shell-python": 394,
+    "receive-netlify-notification-on-telegram": 92,
+    "redesigning-my-website-automation-custom-ssg-optimisations": 153,
+    "testing-pwas-on-mobile-devices-during-development": 44,
+    "the-elder-scrolls-v-skyrim-special-edition-analysis-of-dialogues":19,
+    "tweet2pic-share-tweets-in-form-of-images":37,
+    "using-ci-to-update-static-websites": 115
+}
