d05104a2f3e6365ff5dc02b7586d44756da2cd75
Haider Ali Punjabi <me@haideralipunjabi.com>
2024-11-21 17:35:55 +0530

    wip

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
+  { href: '/posts', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
