b6145d05ac471908373da6c49def02ccd94df663
Haider Ali Punjabi <me@haideralipunjabi.com>
2024-11-23 21:18:17 +0530

    Added GoatCounter

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import { GCScript } from 'next-goatcounter'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -95,6 +96,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
+        <GCScript siteUrl={'https://site.goatcounter.com/count'} />
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
