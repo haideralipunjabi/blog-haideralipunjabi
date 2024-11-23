1255353655f826fe9716d1207be493bc6427a70f
Haider Ali Punjabi <me@haideralipunjabi.com>
2024-11-23 21:58:57 +0530

    fixed goatcounter

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,7 +96,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
-        <GCScript siteUrl={'https://site.goatcounter.com/count'} />
+        <GCScript siteUrl={'https://blog-haideralipunjabi.goatcounter.com/count'} />
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
