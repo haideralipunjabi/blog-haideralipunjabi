bbf2f16592717ab8cd61a84f517b7afe3c2e2b7f
Haider Ali Punjabi <me@haideralipunjabi.com>
2025-04-19 21:47:01 +0530

    added new article on easter egg

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,11 @@ const space_grotesk = Space_Grotesk({
   variable: '--font-space-grotesk',
 })
 
-Sentry.replayIntegration({
-  // NOTE: This will disable built-in masking. Only use this if your site has no sensitive data, or if you've already set up other options for masking or blocking relevant data, such as 'ignore', 'block', 'mask' and 'maskFn'.
-  maskAllText: false,
-  blockAllMedia: false,
-});
+// Sentry.replayIntegration({
+//   // NOTE: This will disable built-in masking. Only use this if your site has no sensitive data, or if you've already set up other options for masking or blocking relevant data, such as 'ignore', 'block', 'mask' and 'maskFn'.
+//   maskAllText: false,
+//   blockAllMedia: false,
+// });
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteMetadata.siteUrl),
--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -8,7 +8,7 @@ Sentry.init({
   dsn: 'https://2e39a005f4115f4e692c2020bec1f955@o4506478515453952.ingest.us.sentry.io/4508341667430400',
 
   // Add optional integrations for additional features
-  integrations: [Sentry.replayIntegration()],
+  // integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
